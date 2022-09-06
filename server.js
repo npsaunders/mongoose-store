32abd98e2e543ea9c19646c3d3b7470bfc282a57
npsaunders <neilpsaunders@gmail.com>
2022-09-06 19:30:04 -0400

    check routes

--- server.js
+++ server.js
@@ -9,16 +9,45 @@ const Product = require('./models/products.js')
 // MIDDLEWARE
 
 // ROUTES
-
+app.get("/", (req, res) => {
+  res.redirect("/products")
+})
 // SEED
 
 // INDEX
+app.get("/products", (req, res) => {
+  res.send("index page")
+})
+
 // NEW
+app.get("/products/new", (req, res) => {
+  res.send("new product")
+})
+
 // DELETE
+app.delete("/products/:id", (req, res) => {
+  res.send("Delete product")
+})
+
 // UPDATE
+app.put("/products/:id", (req, res) => {
+  res.send("update product")
+})
+
 // CREATE
+app.post("/products", (req, res) => {
+  res.send("create product")
+})
+
 // EDIT
+app.get("/products/edit/:id", (req, res) => {
+  res.send("edit product")
+})
+
 // SHOW
+app.get("/products/:id", (req, res) => {
+  res.send("show product")
+})
 
 // LISTENER
 const PORT = process.env.PORT;
