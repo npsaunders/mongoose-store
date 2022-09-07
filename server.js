1217519bedc8b370b8612ceaf004f50e78220db1
npsaunders <neilpsaunders@gmail.com>
2022-09-07 17:45:48 -0400

    fix edit and delete

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const { findById } = require('./models/products.js');
 const Product = require('./models/products.js')
 
 // DATABASE CONNECTION --------------------
@@ -21,6 +23,8 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 // MIDDLEWARE
 // Body parser middleware: give us access to req.body
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 // ROUTES
 app.get("/", (req, res) => {
@@ -61,7 +65,7 @@ app.get('/products', (req, res) => {
   Product.find({}, (error, allProducts) => {
     res.render('index.ejs', {
       products: allProducts,
-      tabTitle: "Index"
+      tabTitle: "Index",
     });
   });
 });
@@ -71,18 +75,24 @@ app.get('/products', (req, res) => {
 app.get("/products/new", (req, res) => {
   res.render("new.ejs", {
     tabTitle: "New"
-  })
-})
+  });
+});
 
 // DELETE
-app.delete("/products/:id", (req, res) => {
-  res.send("Delete product")
-})
+app.delete('/products/:id', (req, res) => {
+  Product.findByIdAndDelete(req.params.id, (err, data) => {
+    res.redirect('/products');
+  });
+});
 
 // UPDATE
 app.put("/products/:id", (req, res) => {
-  res.send("update product")
-})
+  Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true
+  }, (error, updatedProduct) => {
+    res.redirect(`/products/${req.params.id}`);
+  });
+});
 
 // CREATE
 app.post('/products', (req, res) => {
@@ -91,16 +101,25 @@ app.post('/products', (req, res) => {
   });
 });
 
+// EDIT 
+app.get("/products/:id/edit", (req, res) => {
+  Product.findById(req.params.id, (error, foundProduct) => {
+    res.render('edit.ejs', {
+      product: foundProduct,
+      tabTitle: "Edit"
+    });
+  });
+});
 
 // SHOW
 app.get('/products/:id', (req, res) => {
-  Product.findById(req.params.id, (err, foundProduct) => {
+  Product.findById(req.params.id, (error, foundProduct) => {
     res.render('show.ejs', {
       product: foundProduct,
       tabTitle: "Show"
     });
   });
-})
+});
 
 // LISTENER
 const PORT = process.env.PORT;
