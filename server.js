365339664270d4e0dcd037fe3b9762c45608e26c
npsaunders <neilpsaunders@gmail.com>
2022-09-06 21:35:51 -0400

    update view pages and routes to display data

--- server.js
+++ server.js
@@ -2,11 +2,25 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const mongoose = require('mongoose');
 const Product = require('./models/products.js')
 
 // DATABASE CONNECTION --------------------
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Database Connection Error/Success
+// Define callback functions for various events
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 // MIDDLEWARE
+// Body parser middleware: give us access to req.body
+app.use(express.urlencoded({ extended: true }));
 
 // ROUTES
 app.get("/", (req, res) => {
@@ -15,13 +29,21 @@ app.get("/", (req, res) => {
 // SEED
 
 // INDEX
-app.get("/products", (req, res) => {
-  res.send("index page")
-})
+app.get('/products', (req, res) => {
+  Product.find({}, (error, allProducts) => {
+    res.render('index.ejs', {
+      products: allProducts,
+      tabTitle: "Index"
+    });
+  });
+});
+
 
 // NEW
 app.get("/products/new", (req, res) => {
-  res.send("new product")
+  res.render("new.ejs", {
+    tabTitle: "New"
+  })
 })
 
 // DELETE
@@ -35,18 +57,21 @@ app.put("/products/:id", (req, res) => {
 })
 
 // CREATE
-app.post("/products", (req, res) => {
-  res.send("create product")
-})
+app.post('/products', (req, res) => {
+  Product.create(req.body, (error, createdProduct) => {
+    res.redirect('/products');
+  });
+});
 
-// EDIT
-app.get("/products/edit/:id", (req, res) => {
-  res.send("edit product")
-})
 
 // SHOW
-app.get("/products/:id", (req, res) => {
-  res.send("show product")
+app.get('/products/:id', (req, res) => {
+  Product.findById(req.params.id, (err, foundProduct) => {
+    res.render('show.ejs', {
+      product: foundProduct,
+      tabTitle: "Show"
+    });
+  });
 })
 
 // LISTENER
